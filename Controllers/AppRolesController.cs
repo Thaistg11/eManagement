8f8fa7e1f0a8055be6fd1dca63faf4675dd611be
Thais Guide <thaistguide@gmail.com>
2024-08-16 16:03:23 +0100

    I have been add some code in AppRoles File

--- Controllers/AppRolesController.cs
+++ Controllers/AppRolesController.cs
@@ -15,8 +15,22 @@ namespace eManagement.Controllers
         public IActionResult Index()
         {
             var roles = _roleManager.Roles;
-            return View(roles);
-           
+            return View(roles);   
         }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(IdentityRole model)
+        {
+            if (!_roleManager.RoleExistsAsync(model.Name).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(model.Name)).GetAwaiter().GetResult();
+            }
+            return RedirectToAction("Index");
+        }
+
     }
 }
